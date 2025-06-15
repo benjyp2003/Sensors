7e7773e56a739d34a71b06c02ef813e8dff717d2
benjyp2003 <benjypfeffer@gmail.com>
2025-06-15 11:31:30 +0300

    Saved Sensor class.

---
+++ Sensors/Sensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensors
+{
+    internal class Sensor
+    {
+        public string Name {  get; }
+        string Action { get; }  
+        public Sensor(string name, string action)
+        {
+            Name = name;
+            Action = action;
+        }
+    }
+}
