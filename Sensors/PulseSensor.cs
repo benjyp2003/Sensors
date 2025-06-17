300aad2098dab4cb87b45db857405b7a9e9877d9
benjyp2003 <benjypfeffer@gmail.com>
2025-06-17 09:39:26 +0300

    Saved PulseSensor class.

---
+++ Sensors/PulseSensor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensors
+{
+    internal class PulseSensor : ISensor
+    {
+        public string Name { get; }
+        int ActivateCounter { get; set; }
+
+        public PulseSensor(string name)
+        {
+            Name = name;
+            AddToVaulte();
+        }
+
+        public void Activate()
+        {
+            ActivateCounter++;
+            if (ActivateCounter == 3)
+            {
+                BreakSensor();
+            }
+            else
+            {
+                 Console.WriteLine($"The sensor '{Name}' was activated. \n");
+            }
+        }
+
+        void BreakSensor()
+        {
+            Console.WriteLine($"Sensor {Name} is Broken. \n");
+            SensorsVaulte.DeleteSensor(this);
+        }
+
+        public void AddToVaulte()
+        {
+            SensorsVaulte.AddSensorToList(this);
+            Console.WriteLine($"Added sensor '{Name}' to the vault. \n");
+        }
+    }
+}
--- Sensors/Sensors/AudioSensor.cs
+++ Sensors/Sensors/AudioSensor.cs
@@ -18,13 +18,13 @@ namespace Sensors
 
         public void Activate()
         {
-            Console.WriteLine($"The sensor '{Name}' was activated.");
+            Console.WriteLine($"The sensor '{Name}' was activated. \n");
         }
 
         public void AddToVaulte()
         {
             SensorsVaulte.AddSensorToList(this);
-            Console.WriteLine($"Added sensor '{Name}' to the vault.");
+            Console.WriteLine($"Added sensor '{Name}' to the vault. \n");
         }
     }
 }
